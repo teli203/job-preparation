7ffce3e381a3c203681a4bc209fd7fdeec48c130
teli203 <teli203@aol.com>
2024-11-18 08:51:00 -0600

    Create script.js file for index.html and button actions

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('learn-more').addEventListener('click', () => {
+    window.location.href = './pages/interview-prep.html';
+});
+
+document.getElementById('job-boards').addEventListener('click', () => {
+    window.location.href = './pages/job-boards.html';
+});
+
+document.getElementById('apprenti').addEventListener('click', () => {
+    window.location.href = './pages/apprenticeships.html';
+});
+
