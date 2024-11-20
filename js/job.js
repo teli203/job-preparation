33add6ef33cff8f29a4d206d3464870f05645b98
teli203 <teli203@aol.com>
2024-11-20 00:24:06 -0600

    Create event listener for 2 out of 3 of the buttons on the page

---
+++ js/job.js
@@ -0,0 +1,7 @@
+document.getElementById('find').addEventListener('click', () => {
+    window.location.href = 'https://www.findmyprofession.com/career-advice/best-job-search-sites/';
+});
+
+document.getElementById('founder').addEventListener('click', () => {
+    window.location.href = 'https://www.founderjar.com/best-job-search-sites/';
+});
