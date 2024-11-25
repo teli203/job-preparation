c6c40f1787ec5d06983a41d2c6d233b81dcca343
teli203 <teli203@aol.com>
2024-11-25 14:05:03 -0600

    Add comment note for arrow section

--- js/job.js
+++ js/job.js
@@ -68,6 +68,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// UP ARROW SCROLL EVENT //
 
 const scrollImage = document.getElementById('scrollImage');
 
